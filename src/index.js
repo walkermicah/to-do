96366f2cddb0c254e93f39bfedf22eae8df53399
Walker <walker.m.oliver@gmail.com>
2022-08-17 10:26:28 -0400

    Initialize project directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import { ProjectDirectory } from "./projectDirectory.js";
+
+const directory = new ProjectDirectory();
+
+//ADD PROJECT
+directory.addProject("Project 1");
+//ADD TASK
+directory.getProject(1).addTask(1, 1, 1, 1, 1, 1, 1);
+//DELETE PROJECT
+directory.deleteProject(1);
