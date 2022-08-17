2db7a4f7d03bac75e3bf099e1f6c5379072fc549
Walker <walker.m.oliver@gmail.com>
2022-08-17 09:23:42 -0400

    Initial commit

---
+++ src/project.js
@@ -0,0 +1,33 @@
+import { Task } from "./task.js";
+
+export class Project {
+  constructor(title) {
+    this.title = title;
+  }
+
+  tasks = [];
+
+  editTitle(newTitle) {
+    this.title = newTitle;
+    return this.title;
+  }
+
+  addTask(project, title, description, dueDate, notes, priority, complete) {
+    const newTask = new Task(
+      project,
+      title,
+      description,
+      dueDate,
+      notes,
+      priority,
+      complete
+    );
+    this.tasks.push(newTask);
+  }
+
+  deleteTask(taskID) {
+    const taskToDelete = this.tasks.find((task) => task.id === taskID);
+    const index = this.tasks.indexOf(taskToDelete);
+    this.tasks.splice(index, 1);
+  }
+}
