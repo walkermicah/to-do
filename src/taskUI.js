469e44b3a8c18541f7e581d8d204ec9a0d871dbc
Walker <walker.m.oliver@gmail.com>
2022-08-25 15:04:38 -0400

    Remove drop down and add active project variable

--- src/taskUI.js
+++ src/taskUI.js
@@ -3,56 +3,16 @@ import Delete from "./img/delete.svg";
 
 const taskMenu = document.querySelector(".taskbar-menu");
 const taskForm = document.querySelector(".taskbar-form");
-const projectDropDown = document.querySelector(".slide");
 const tasklist = document.querySelector(".tasklist");
+const taskName = document.querySelector(".task-name-input");
+
+let activeProject;
 
 export class TaskUI {
   static appendElement(parent, el, ...classList) {
     const element = parent.appendChild(document.createElement(`${el}`));
     element.classList.add(`${classList}`);
     return element;
-  }
-
-  ///////////// TASKBAR
-  //show names of projects in drop-down menu
-  static populateDropdown(directory) {
-    directory.projects.forEach((proj) => {
-      const li = projectDropDown.appendChild(document.createElement("li"));
-      const colorIndicator = li.appendChild(document.createElement("div"));
-      colorIndicator.classList.add("project-color");
-      colorIndicator.style.backgroundColor = proj.color;
-      const link = li.appendChild(document.createElement("a"));
-      link.href = "#";
-      link.textContent = proj.title;
-    });
-  }
-
-  //clear projects from drop down when form exits
-  static clearDropdown() {
-    const menu = document.querySelector(".slide");
-    while (menu.firstChild) {
-      menu.removeChild(menu.lastChild);
-    }
-  }
-
-  //show form to add new task
-  static showNewTaskForm(directory) {
-    taskMenu.classList.add("hidden");
-    taskForm.classList.remove("hidden");
-    this.populateDropdown(directory);
-  }
-
-  static submitNewTask(e) {
-    e.preventDefault();
-  }
-
-  //exit new task form
-  static exitNewTaskForm(e) {
-    e.preventDefault();
-    taskForm.reset();
-    this.clearDropdown();
-    taskForm.classList.add("hidden");
-    taskMenu.classList.remove("hidden");
   }
 
   ///////////// TASK TABS
@@ -64,8 +24,8 @@ export class TaskUI {
   }
 
   //add checkbox to task
-  static appendCheckbox(task) {
-    const checkbox = task.appendChild(document.createElement("label"));
+  static appendCheckbox(el) {
+    const checkbox = el.appendChild(document.createElement("label"));
     checkbox.classList.add("task-checkbox");
     const input = checkbox.appendChild(document.createElement("input"));
     input.setAttribute("type", "checkbox");
@@ -74,45 +34,45 @@ export class TaskUI {
   }
 
   //add star to task
-  static appendStar(task) {
-    const star = task.appendChild(document.createElement("input"));
+  static appendStar(el) {
+    const star = el.appendChild(document.createElement("input"));
     star.classList.add("star");
     star.setAttribute("type", "checkbox");
   }
 
   //add title to task
-  static appendTitle(task, t) {
-    const div = task.appendChild(document.createElement("div"));
+  static appendTitle(el, title) {
+    const div = el.appendChild(document.createElement("div"));
     div.classList.add("task-title");
-    div.textContent = t.title;
+    div.textContent = title;
   }
 
   //add notes to task
-  static appendNotes(task, t) {
-    const div = task.appendChild(document.createElement("div"));
+  static appendNotes(el, notes) {
+    const div = el.appendChild(document.createElement("div"));
     div.classList.add("task-notes");
-    div.textContent = t.notes;
+    div.textContent = notes;
   }
 
   //add date to task
-  static appendDate(task, t) {
-    const div = task.appendChild(document.createElement("div"));
+  static appendDate(el, date) {
+    const div = el.appendChild(document.createElement("div"));
     div.classList.add("task-date");
-    div.textContent = t.date;
+    div.textContent = date;
   }
 
   //append delete button to task
-  static appendBtn(task, src, classList) {
-    const btn = task.appendChild(document.createElement("button"));
+  static appendBtn(el, src, classList) {
+    const btn = el.appendChild(document.createElement("button"));
     const img = btn.appendChild(document.createElement("img"));
     img.src = src;
     img.classList.add(`${classList}`, "icon-small");
   }
 
   //color code task
-  static colorCode(task, project) {
+  static colorCode(el, project) {
     const projectColor = project.color;
-    task.style.borderLeft = `10px solid ${projectColor}`;
+    el.style.borderLeft = `10px solid ${projectColor}`;
   }
 
   //load tasklist
@@ -123,6 +83,7 @@ export class TaskUI {
       e.target.closest(".project-tab").dataset.id
     );
     const tasks = project.tasks;
+    activeProject = project;
 
     //display tasks
     tasks.forEach((t) => {
@@ -133,12 +94,90 @@ export class TaskUI {
       //add elements to each task
       this.appendCheckbox(task);
       this.appendStar(task);
-      this.appendTitle(task, t);
-      this.appendNotes(task, t);
-      this.appendDate(task, t);
+      this.appendTitle(task, t.title);
+      this.appendNotes(task, t.notes);
+      this.appendDate(task, t.date);
       this.appendBtn(task, Edit, "task-edit");
       this.appendBtn(task, Delete, "task-delete");
       this.colorCode(task, project);
     });
+  }
+
+  ///////////// TASKBAR
+  //show names of projects in drop-down menu
+  // static populateDropdown(directory) {
+  //   directory.projects.forEach((proj) => {
+  //     const li = projectDropDown.appendChild(document.createElement("li"));
+  //     li.setAttribute("data-id", proj.id);
+  //     const colorIndicator = li.appendChild(document.createElement("div"));
+  //     colorIndicator.classList.add("dropdown-color");
+  //     colorIndicator.style.backgroundColor = proj.color;
+  //     const link = li.appendChild(document.createElement("a"));
+  //     link.href = "#";
+  //     link.textContent = proj.title;
+  //   });
+  // }
+
+  //clear projects from drop down when form exits
+  // static clearDropdown() {
+  //   const menu = document.querySelector(".slide");
+  //   while (menu.firstChild) {
+  //     menu.removeChild(menu.lastChild);
+  //   }
+  // }
+
+  //allow user to select project from drop-down
+  // static selectProject(e, directory) {
+  //   const project = directory.getProjectByID(e.target.closest("li").dataset.id);
+  //   dropDownLabel.setAttribute("data-id", project.id);
+  //   dropDownLabel.textContent = project.title;
+  // }
+
+  //show form to add new task
+  static showNewTaskForm(directory, e) {
+    e.preventDefault();
+    taskMenu.classList.add("hidden");
+    taskForm.classList.remove("hidden");
+    taskName.focus();
+    // this.populateDropdown(directory);
+  }
+
+  //create new task
+  static submitNewTask(e, directory) {
+    e.preventDefault();
+    const title = taskName.value;
+    const notes = document.querySelector(".task-notes-input").value;
+    const date = document.querySelector(".task-date-input").value;
+
+    if (!activeProject) return;
+    if (!title) return;
+
+    activeProject.addTask(`${title}`, `${notes}`, `${date}`);
+
+    //add task to display
+    const task = tasklist.appendChild(document.createElement("div"));
+    task.classList.add("task");
+
+    //add elements to each task
+    this.appendCheckbox(task);
+    this.appendStar(task);
+    this.appendTitle(task, title);
+    this.appendNotes(task, notes);
+    this.appendDate(task, date);
+    this.appendBtn(task, Edit, "task-edit");
+    this.appendBtn(task, Delete, "task-delete");
+    this.colorCode(task, activeProject);
+
+    taskForm.reset();
+    taskName.focus();
+  }
+
+  //exit new task form
+  static exitNewTaskForm(e) {
+    e.preventDefault();
+    taskForm.reset();
+    this.clearDropdown();
+    taskForm.classList.add("hidden");
+    taskMenu.classList.remove("hidden");
   }
 }
